812c0575806226e8e5c6e0f26d1d1d0e093688bc
Deplais Vincent <vincent.deplais@orange.fr>
2017-12-12 18:24:48 +0100

    Update template

--- back/views/room_publique.php
+++ back/views/room_publique.php
@@ -1,11 +1,20 @@
-<main>
-    <form>
-        <input type="text" id="m" style="background:red" autofocus>
-        <input type="submit" value="Send">
-    </form>
+<main class="wrapper-page-room">
+    <section class="section-room-friends">
+        <div class="wrapper-onglets">
+            <ul>
+                <li class="selected"><a href="#">Salons</a></li>
+                <li><a href="#">Amis</a></li>
+            </ul>
+        </div>
+    </section>
 
-    <ul id="messages"></ul>
+    <section class="section-chat">
+        <div class="header-chat">
 
-    <section class="section-chat"></section>
+        </div>
+        <div class="wrapper-chat">
+            <ul id="message-chat">
+            </ul>
+        </div>
+    </section>
 </main>
-    
