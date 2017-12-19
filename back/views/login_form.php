72a36ca4cd19ad54da3b7d33a62fcb081f785c0e
Unknown <elmar.atavares@gmail.com>
2017-12-19 11:37:35 +0100

    Fix log in ? column saving user in database & add required at forms

--- back/views/login_form.php
+++ back/views/login_form.php
@@ -4,10 +4,10 @@
   </h1>
   <div class="container">
     <label><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <input type="text" placeholder="Enter Username" name="uname" required="required" pattern="[A-Za-z0-9]{1,20}">
 
     <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" required="required" pattern="[A-Za-z0-9]{1,20}">
 
     <button type="submit">Login</button>
    
