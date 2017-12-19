d13baede8d1bf74b0dfc8920fde99938df7b40ff
Unknown <elmar.atavares@gmail.com>
2017-12-19 13:04:58 +0100

    Move kill action in authController

--- back/controllers/AuthController.php
+++ back/controllers/AuthController.php
@@ -64,7 +64,21 @@ switch ($action) {
         break;
 
     case 'kill':
-        # code...
+
+        // On détruit les variables de notre  et on met à false l'attribut login dans la base de donnée
+
+        $currentUser = $_SESSION['currentUser'];
+        $currentUser->login = false;
+        $currentUser->save();
+
+        session_unset();
+
+        //On détruit notre session
+        session_destroy ();
+
+        // On redirige le visiteur vers la page d'accueil
+        header ('location: index.php');
+        
         break;
     
     default:
--- back/views/kill.php
+++ back/views/kill.php
@@ -1,18 +1,2 @@
 <?php
-// On démarre la session
-//session_start ();
-
-// On détruit les variables de notre  et on met à false l'attribut login dans la base de donnée
-
-$currentUser = $_SESSION['currentUser'];
-$currentUser->login = false;
-$currentUser->save();
-
-session_unset();
-
-//On détruit notre session
-session_destroy ();
-
-// On redirige le visiteur vers la page d'accueil
-header ('location: index.php');
 ?>
